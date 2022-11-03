d44a1cc9fd54b9764c5d16e02366c2721f2d9b7d
Ankur Jain <ankur.jain@solana.com>
2022-11-03 10:05:25 +0000

    Update balance on account auth

--- mobile-wallet-adapter-kotlin/app/src/main/java/com/solanamobile/mwaworkshop/MainViewModel.kt
+++ mobile-wallet-adapter-kotlin/app/src/main/java/com/solanamobile/mwaworkshop/MainViewModel.kt
@@ -105,6 +105,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                         ),
                     )
                 }
+                updateAccountBalance()
             }
         }
     }
